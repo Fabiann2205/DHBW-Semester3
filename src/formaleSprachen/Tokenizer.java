95369c516fb5ae8c365a1f3e47d5ce3a4a916f82
Fabian Specht <fabian.specht@sap.com>
2023-10-04 10:09:45 +0200

    init

--- src/formaleSprachen/Tokenizer.java
+++ src/formaleSprachen/Tokenizer.java
@@ -1,51 +1,44 @@
 package formaleSprachen;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 public class Tokenizer {
 
-	public static List<Token> analyze( String input ){
-		List<Token> result = new ArrayList<>();
+    public static List<Token> analyze(String input) {
+        List<Token> result = new ArrayList<>();
 
-		String[] split = input.split(" ");
-		List<String> splitList = List.of(split);
-		boolean isNumber = false;
-		for(String a : splitList) {
-			if(isNumber) {
-				result.add(new Token(Token.Type.NUMBER, a));
-				continue;
-			}
-			String aNew = a.toUpperCase();
-			switch (aNew) {
-				case "SELECT":
-					result.add(new Token(Token.Type.PROJECTION, a));
-					break;
-				case "AS":
-					result.add(new Token(Token.Type.ALIAS, a));
-					break;
-				case "FROM":
-					result.add(new Token(Token.Type.SOURCE, a));
-					break;
-				case "WHERE":
-					result.add(new Token(Token.Type.CONDITION, a));
-					break;
-				case ">", "<", "=":
-					result.add(new Token(Token.Type.GT, a));
-					isNumber = true;
-					break;
-				default:
-					result.add(new Token(Token.Type.IDENTIFIER, a));
-					break;
+        String[] split = input.split(" ");
+        List<String> splitList = List.of(split);
+        boolean isNumber = false;
+        for (String a : splitList) {
+            if (isNumber) {
+                result.add(new Token(Token.Type.NUMBER, a));
+                continue;
             }
-		}
-		return result;
-	}
-
-	public static void main(String[] args) {
-		List<Token> ergebnis = analyze("Hallo das ist ein Test");
-		System.out.println("nfjg");
-	}
-	
+            String aNew = a.toUpperCase();
+            switch (aNew) {
+                case "SELECT":
+                    result.add(new Token(Token.Type.PROJECTION, a));
+                    break;
+                case "AS":
+                    result.add(new Token(Token.Type.ALIAS, a));
+                    break;
+                case "FROM":
+                    result.add(new Token(Token.Type.SOURCE, a));
+                    break;
+                case "WHERE":
+                    result.add(new Token(Token.Type.CONDITION, a));
+                    break;
+                case ">", "<", "=":
+                    result.add(new Token(Token.Type.GT, a));
+                    isNumber = true;
+                    break;
+                default:
+                    result.add(new Token(Token.Type.IDENTIFIER, a));
+                    break;
+            }
+        }
+        return result;
+    }
 }
